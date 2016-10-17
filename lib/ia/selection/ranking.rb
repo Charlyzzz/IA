3f4c902fd5b2423bc50db42972b115906077b5c6
Charlyzzz <erwincdl@gmail.com>
2016-10-17 04:04:55 -0300

    Mensajes de progreso para el binario y binario compilado

--- lib/ia/selection/ranking.rb
+++ lib/ia/selection/ranking.rb
@@ -2,7 +2,9 @@ module IA
   module Ranking
     class << self
       def sort(population)
-        population.sort_by { |train| train.fitness }
+        population
+            .sort_by { |train| train.fitness }
+            .reverse
       end
     end
   end
--- scripts/solution_script.rb
+++ scripts/solution_script.rb
@@ -13,7 +13,16 @@ require 'csv'
 # Creación de la población inicial = Aleatoria
 # Resultado de la función aptitud máxima = 160
 
-results = Array.new(3) { IA::GeneticAlgorithm.new.reach_fitness(160) }
+puts 'Starting simulation, please wait.'
+
+results = Array.new(3) do
+  solution = IA::GeneticAlgorithm.new.reach_fitness(160)
+  puts 'Run finished.'
+  solution
+end
+
+puts 'All runs have finished.'
+puts 'Outputting results.'
 
 results
     .map { |result| result[:metadata] }
@@ -21,5 +30,6 @@ results
     .each_with_index { |csv, index| File.write("results_run_#{index + 1}.txt", csv) }
 
 content = results.map { |hash| [hash[:winner], hash[:iteration], hash[:last_population]] }.inspect
-
 File.write('results.txt', content)
+Dir.entries('.').grep(/.+\.txt/).each { |file| puts "#{file} was created." }
+puts 'End of simulation.'
